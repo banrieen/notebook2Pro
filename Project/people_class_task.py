d2435cab6bd89b9449393a1f050507c6e4a3b0db
Banrieen <banrieen@163.com>
2018-03-19 07:03:14 +0800

    Update the test example,remove the un-used,un-finished

--- Project/people_class_task.py
+++ Project/people_class_task.py
@@ -1,28 +1,107 @@
-
+#! /usr/bin/env python
 # -*- coding: UTF-8 -*-
-'''
-Created on 2018年03月19日
-@author: banrieen
-'''
-
+from __future__ import division
 
 class People:
 
-    def __init__(self,people_name,people_age):
-        self.name = people_name
-        self.age = people_age
-
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
     def getName(self):
         return self.name
-    def getName(self):
+    
+    def getAge(self):
         return self.age
 
-def HiHello():
-    print ("Hello ,It is a fine !")
+class Student(People):
+    
+    def __init__(self,name,age,id):
+        People.__init__(self, name, age)
+        self.id = id
+        
+    def getId(self):
+        return self.id
 
+class Xdict(dict):
+    
+    def __init__(self,new_dict):
+        dict.__init__(self)
+        self.new_dict = dict(new_dict)
+        
+    def getKeys(self,value):
+        if value in self.new_dict.values():
+            key_list = []
+            for ikey in self.new_dict.keys():
+                if value in self.new_dict[ikey]:
+                    key_list.append(ikey)
+            return key_list
+        else:
+            return None
+
+        
+class Vector:
+    
+    def __init__(self,x,y,z):
+        self.vt = (x,y,z)
+    
+    def __add__(self,other):
+        return self.format(map(sum,zip(self.vt,other.vt)))
+        
+    def sub(self,si):
+        return si[0] - si[1]
+    
+    def __sub__(self,other):     
+        return self.format(map(self.sub,zip(self.vt,other.vt)))
+        
+    def mul(self,si):
+        return si[0] * si[1]
+    
+    def div(self,si):
+        return si[0] / si[1]
+    
+    def __mul__(self,other):
+        return self.format(map(self.mul,zip(self.vt,[other]*len(self.vt))))
+
+    
+    def __truediv__(self,other):
+        return self.format(map(self.div,zip(self.vt,[other]*len(self.vt))))
+    
+    def format(self,mps):
+        for i in range(len(mps)):
+            mps[i] = float("{:.1f}".format(mps[i]))
+        return str(tuple(mps))
+    
+    def __str__(self): 
+        for i in range(len(self.vt)):
+            self.vt[i] = float("{:.1f}".format(self.vt[i]))     
+        return str(tuple(self.vt))
+        
+
+        
 if __name__ == '__main__':
-    people_name = input("Please input the people`s name: ")
-    people_age = input("Please input the people`s age (int): ")
+    alist = input().split()
+    blist = input().split()
+    n = float(input())
+    
+    a = Vector(float(alist[0]),float(alist[1]),float(alist[2]))    
+    b = Vector(float(blist[0]),float(blist[1]),float(blist[2]))    
+    
+    print "{} {} {} {}".format(a+b,a-b,a*n,a/n)
 
-    P = People(people_name,people_age)
-    print(P.getName, P.getAge())
+ #==============================================================================
+ #    name = input()
+ #    age = int(input())
+ #    id = input()
+ #    
+ #    S = Student(name,age,id)
+ #    print("{} {} {}".format(S.getName(), S.getAge(),S.getId()))
+ # 
+ #    p = People(name,age)
+ #    print("{} {}".format(p.getName(),p.getAge()))
+ #    alist = list(input().split())
+ #    blist = list(input().split())
+ #    target = input()
+ #    d = Xdict(zip(alist,blist))
+ #    print(sorted(d.getKeys(target)))
+ #==============================================================================
