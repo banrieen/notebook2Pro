fad60ecab01279830962dcabb7f0c47f34ba0970
huizhi <banrieen@163.com>
2019-05-20 09:54:01 +0800

    使用配置文件

--- deplyJupyter.py
+++ deplyJupyter.py
@@ -2,4 +2,5 @@
 
 # Start jupyter notebook
 # jupyter notebook --generate-config /root/.ipython/profile_default/ipython_kernel_config.py 
+mv jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
 nohup jupyter notebook --allow-root --ip=0.0.0.0 --port=8888 --notebook-dir=/home/notebook &
