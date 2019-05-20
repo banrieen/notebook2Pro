302ab6965a2de181dfa4bf902fa5169ba5fc801d
huizhi <banrieen@163.com>
2019-05-20 09:05:18 +0800

    update notebook filepath

--- deplyJupyter.py
+++ deplyJupyter.py
@@ -2,4 +2,4 @@
 
 # Start jupyter notebook
 # jupyter notebook --generate-config /root/.ipython/profile_default/ipython_kernel_config.py 
-nohup jupyter notebook --allow-root --ip=0.0.0.0 --port=8888 --notebook-dir=/home/exercise-notebook &
+nohup jupyter notebook --allow-root --ip=0.0.0.0 --port=8888 --notebook-dir=/home/notebook &
