4aa429902c3ccdbf7c0db3082de67558bb3aaf2d
Banrieen <banrieen@163.com>
2019-05-22 15:54:42 +0800

    unstage changes

---
+++ idea_sea_rchdemo/src/main/java/com/example/demo/IdeaSeaRchdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaSeaRchdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaSeaRchdemoApplication.class, args);
+	}
+}
