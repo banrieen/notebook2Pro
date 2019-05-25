f2a5f37e1a6c62baecff81eb5d1be16fe97361d9
Banrieen <banrieen@163.com>
2019-05-25 23:46:10 +0800

    Update deploy.sh

--- deplyJupyter.sh
+++ deplyJupyter.sh
@@ -1,6 +1,6 @@
 # Auto deploy services
 
 # Start jupyter notebook
-# jupyter notebook --generate-config /root/.ipython/profile_default/ipython_kernel_config.py 
-cp -u -f jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
-nohup jupyter notebook --allow-root --ip=0.0.0.0 --port=8888 --notebook-dir=/home/notebook &
+# jupyter notebook --generate-config /root/.juyper/jupyter_notebook_config.py 
+# nohup jupyter notebook --allow-root --ip=0.0.0.0 --port=8888 --notebook-dir=/home/notebook &
+nohup jupyter notebook --config  jupyter_notebook_config.py &
