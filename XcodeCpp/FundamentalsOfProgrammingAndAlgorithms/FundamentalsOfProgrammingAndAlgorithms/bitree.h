85754d6786b7bddd269a6b026a4ea6b564ea135c
Banrieen <banrieen@163.com>
2019-11-15 07:58:05 +0800

    Update xcode project

---
+++ XcodeCpp/FundamentalsOfProgrammingAndAlgorithms/FundamentalsOfProgrammingAndAlgorithms/bitree.h
@@ -0,0 +1,57 @@
+//
+//  bitree.h
+//  FundamentalsOfProgrammingAndAlgorithms
+//
+//  Created by lizhen on 2019/10/13.
+//  Copyright © 2019 lizhen. All rights reserved.
+//
+
+#ifndef bitree_h
+#define bitree_h
+
+#include <stdio.h>
+
+
+
+
+// Define a struct for binary tree nodes.
+typedef struct BiTreeNode_
+{
+    void *data;
+    struct BiTreeNode_ *left;
+    struct BiTreeNode_ *right;
+}BiTreenode;
+
+// Define a struct for binary trees.
+typedef struct BiTree_{
+    int size;
+    int (*compare)(const void *key1, const void *key2);
+    void (*destory)(void *data);
+    BiTreeNode *root;
+}BiTreee;
+
+// Public interface
+void bitree_init(BiTree *tree, void (*destory)(void *data)));
+void bitree_destory(BiTree *tree);
+int bitree_ins_left(BiTree *tree, BiTreeNode *node, const void *data);
+int bitree_ins_right(BiTree *tree, BiTreeNode *node, const void *data);
+void bitree_rem_left(BiTree *tree, BiTreeNode *node);
+void bitree_rem_right(BiTree *tree, BiTreeNode *node);
+int bitree_merge(BiTree *merge, BiTree *left, BiTree *right, const void *data);
+
+#define bitree_size(tree) ((tree)->size)
+
+#define bitree_root(tree) ((tree)->root)
+
+#define bitree_is_eob(node) ((node) == NULL)
+
+#define bitree_is_leaf(node) ((node)->left == NULL && (node)->right == NULL)
+
+#define bitree_data(node) ((node)->data)
+
+#define bitree_left(node) ((node)->left)
+
+#define bitree_right(node) ((node)->right)
+
+#endif
+#endif /* bitree_h */
