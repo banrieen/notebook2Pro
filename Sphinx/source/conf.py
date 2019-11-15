f4b6c540235722638df2e6c9e48b42c5cec9d3fb
anien <banrieen@163.com>
2019-11-15 19:43:59 +0800

    Rename ipynb extension name

--- Sphinx/source/conf.py
+++ Sphinx/source/conf.py
@@ -48,7 +48,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx.ext.nbsphinx',
+    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
